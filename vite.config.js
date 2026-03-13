4b8955ef148ed841394ec0c6ea0c2b06c5d1b73b
Tseng Wen-yen <wenyen@example.com>
2026-03-13 14:21:14 +0800

    fix: add base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // 引入插件
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // 啟用插件
   ],
+  // 加入這一行，確保在 GitHub Pages 上路徑正確
+  base: '/jp_quiz_app/',
 })
